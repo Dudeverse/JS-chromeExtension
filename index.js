cd118aeb950ebb3f6338558dcc9f6fe62b9a0925
Dudeverse <se352@njit.edu>
2024-01-21 09:36:24 -0500

    removed comments

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-// Refector the function so that it takes a parameter, leads, that it uses
-// instead of the global myLeads variable. Remember to update all invocations 
-// of the function as well.
-
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
